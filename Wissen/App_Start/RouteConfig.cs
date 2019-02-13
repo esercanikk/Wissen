c76b5cfaf2e7d298d55def66ec4aeff0718d2fbc
Eser Canik <esercanik@hotmail.com>
2019-02-13 12:38:51 +0300

    tamamlandı- ContactPages

--- Wissen/App_Start/RouteConfig.cs
+++ Wissen/App_Start/RouteConfig.cs
@@ -12,7 +12,10 @@ namespace Wissen
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(name: "About", url: "Hakkimizda",
+                defaults: new { controller = "Home", action = "About" });
+            routes.MapRoute(name: "Contact", url: "İletişim",
+                defaults: new { controller = "Home", action = "Contact" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
